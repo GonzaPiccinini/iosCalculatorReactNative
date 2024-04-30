b2084010cc25b153aea802648d41ade74fcc58d5
Gonzalo Piccinini <gonzipiccinini@gmail.com>
2024-04-30 00:37:52 -0300

    designed finished

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+  StatusBar,
+  View,
+} from 'react-native';
+import { CalculatorScreen } from './presentation/screens/CalculatorScreen';
+import { styles } from './config/appTheme';
+
+
+function App() {
+  return (
+    <View style={styles.background}>
+      <StatusBar
+        barStyle={'light-content'}
+        backgroundColor={'black'}
+      />
+      <CalculatorScreen />
+    </View>
+  );
+}
+
+export default App;
